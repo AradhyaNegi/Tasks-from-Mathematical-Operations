f5c96258894ec8f5c797a8b4310f5bc8778636d8
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-26 15:48:34 +0530

    Add files via upload

---
+++ Tasks of- Mathematical Operations.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Tasks Of Mathematical Operations-2
+"""
+#TASK 1
+r=5
+a=3.14*(r**2)
+print(a)
+
+#TASK 3
+km=50
+m=km*1000
+print(m)
+
+#TASK 4
+r=15
+h=15
+volume = 3.14*(r**2)*h
+print(volume)
+
+#TASk 5
+a=45%12
+print(a)
+
