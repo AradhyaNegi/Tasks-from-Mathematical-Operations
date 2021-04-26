60110e6f8c3a91cb8bdc2067234be08779b4de1c
Aradhya Negi <83121409+AradhyaNegi@users.noreply.github.com>
2021-04-26 16:30:44 +0530

    Add files via upload

---
+++ Tasks of- Mathematical Operations.py part 2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Tasks-from-Mathematical-Operations /2
+Use of Operators
+"""
+# TASK 1
+Name= " Aradhya "
+Sirname=" Negi "
+print("Your full name is  : "  +  Name  +  Sirname )
+
+#TASK 2
+numaaa=Name * 20
+print(numaaa)
+
+#TASK 3
+lol="I am INVENTOR "
+lol_= lol * 10
+print(lol_)
+
